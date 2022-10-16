a992de35499df505c4115b16a504fed38d95a25a
ViktoriyaKo <kozlova_viktoriy@mail.ru>
2022-10-16 12:28:48 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -17,9 +17,44 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 // НЕ РЕШЕНА
 function repeater(str, options) {
-  return str.repeat(options.repeatTimes);
-}
+  let string = String(str),
+    additionRepeatTimes = options.additionRepeatTimes || 1,
+    repeatTimes = options.repeatTimes || 1,
+    separator = options.separator || "+",
+    addition = String(options.addition);
+  additionSeparator = options.additionSeparator || "|";
 
+  let str1 = "";
+  if (addition === "undefined") {
+    for (let i = 0; i < repeatTimes; i++) {
+      str1 += string;
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        str1 += "";
+        if (j !== additionRepeatTimes - 1) {
+          str1 += additionSeparator;
+        }
+      }
+      if (i !== repeatTimes - 1) {
+        str1 += separator;
+      }
+    }
+  } else {
+    for (let i = 0; i < repeatTimes; i++) {
+      str1 += string;
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        str1 += addition;
+        if (j !== additionRepeatTimes - 1) {
+          str1 += additionSeparator;
+        }
+      }
+      if (i !== repeatTimes - 1) {
+        str1 += separator;
+      }
+    }
+  }
+
+  return str1;
+}
 module.exports = {
   repeater,
 };
